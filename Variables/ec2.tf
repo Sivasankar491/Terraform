0e111102cd935790a64229962fd6f14d522124e8
Sankar491 <sivasankarrcks@gmail.com>
2024-09-12 13:48:09 +0530

    Using vars.tf file

--- Variables/ec2.tf
+++ Variables/ec2.tf
@@ -1,12 +1,16 @@
 resource "aws_instance" "test" {
     ami = var.ami_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.ssh.id]
+    #vpc_security_group_ids = [aws_security_group.ssh.id]
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = var.tags
 }
 
-resource "aws_security_group" "ssh" {
-    
+#resource "aws_security_group" "ssh" {
+resource "aws_security_group" "allow_ssh_terraform" {
+    name  = "allow_sshh" #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
+
      egress {
         from_port        = 0
         to_port          = 0
@@ -18,7 +22,7 @@ resource "aws_security_group" "ssh" {
      ingress {
         from_port        = var.from_port
         to_port          = var.to_port
-        protocol         = "var.protocol"
+        protocol         = var.protocol
         cidr_blocks      = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
   }
