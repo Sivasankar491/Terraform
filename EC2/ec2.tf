2a5fcf7419dd068b85a520d903f03fd3ac5de258
Sankar491 <sivasankarrcks@gmail.com>
2024-09-12 13:40:43 +0530

    creating ec2 instance

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,33 +1,36 @@
-resource "aws_security_group" "allow_ssh" {
 
-    name = "Allow_ssh"
-    description = "It allows connection on port 22 from everywhere"
-
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-  }
-   
-    ingress {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-  } 
-}
-
-resource "aws_instance" "test_instance" {
+resource "aws_instance" "terraform" {
 
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
-    security_groups = [aws_security_group.allow_ssh.id]
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
+}
+
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "allow_sshh" #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
+
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
 
     tags = {
-      name = "Test"
+        Name = "allow_sshh"
     }
-  
 }
