89a14b638e4f51f5da6c7e756c3123dfe0139eb4
Sankar491 <sivasankarrcks@gmail.com>
2024-09-16 21:50:20 +0530

    getting private and public ip addresses for multiple instances using output

--- Outputs/ec2.tf
+++ Outputs/ec2.tf
@@ -1,11 +1,11 @@
 
 resource "aws_instance" "terraform" {
-    
+    count = length(var.instance_names)
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
-        Name = "terraform"
+        Name = var.instance_names[count.index]
     }
 }
 
--- Outputs/outputs.tf
+++ Outputs/outputs.tf
@@ -1,3 +1,7 @@
-output "instance_info" {
-    value = aws_instance.terraform.private_ip
+output "private_ip_info" {
+    value = aws_instance.terraform[*].private_ip
+}
+
+output "public_ip_info" {
+    value = aws_instance.terraform[*].public_ip
 }
