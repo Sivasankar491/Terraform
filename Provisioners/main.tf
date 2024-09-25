49251ed524fda6d680ea0d24dbead3d3e9ac183f
Sankar491 <sivasankarrcks@gmail.com>
2024-09-25 23:10:25 +0530

    Provisioners work only at the time of resource creation

--- Provisioners/main.tf
+++ Provisioners/main.tf
@@ -37,4 +37,25 @@ resource "aws_instance" "provison_ec2" {
        #command = "echo ${aws_instance.provison_ec2.private_ip} > private.txt"
        command = "echo ${self.public_ip} > public.txt"
     }
+    
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx",
+      ]
+    }
+
+    provisioner "remote-exec" {
+      when    = destroy
+      inline = [
+          "sudo systemctl stop nginx",
+      ]
+    }
 }
